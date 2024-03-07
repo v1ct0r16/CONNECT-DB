17212115efeedf4af73809ddd6a5c4e9cc67c674
v1ct0r16 <pvictor1844@gmail.com>
2024-03-07 12:15:26 +0100

    started off by downloading my provider and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { SaleController } from './sale/sale.controller';
+import { Sale } from './sale/sale';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost/nest')],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [MongooseModule.forRoot('mongodb+srv://VictorPromise:pvictor1844@cluster0.2ckcmwb.mongodb.net/')],
+  controllers: [AppController, SaleController],
+  providers: [AppService, Sale],
 })
 export class AppModule {}
