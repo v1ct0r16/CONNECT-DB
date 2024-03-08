1f9a01964b0efa8767b9baec71a997396d3f5927
v1ct0r16 <pvictor1844@gmail.com>
2024-03-08 15:01:34 +0100

    second commit for the day

--- src/sale/sale.service.ts
+++ src/sale/sale.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateSaleDto } from 'src/dto/sale.dto';
@@ -10,6 +10,23 @@ export class SaleService {
         @InjectModel(Sale.name) private readonly saleModel: Model<Sale>
         ) { }
 
+async addSale(payload:CreateSaleDto){
+    const saveItem = new this.saleModel(payload);
+    return await saveItem.save()
+}
+async getSaleById(id: string) {
+    try {
+        const find = await  this.saleModel.findById(id).exec();
+        if (!find) {
+            throw new NotFoundException(`Item with ID ${id} not found`);
+        }
+
+    return find;
+    } catch (error) {
+        throw new NotFoundException(`Item with ID ${id} not found`);
+    
+    }
+}
 
 }
 
