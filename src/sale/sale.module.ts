ef7039ef22803d99e1e451eb5285cf446be72016
v1ct0r16 <pvictor1844@gmail.com>
2024-03-08 15:39:36 +0100

    just fixed a bug, crazy i know

--- src/sale/sale.module.ts
+++ src/sale/sale.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { SaleProvider } from './sale.service';
+import {SaleProvider } from './sale.service';
 
 @Module({
   providers: [SaleProvider]
--- src/sale/sale.service.ts
+++ src/sale/sale.service.ts
@@ -6,51 +6,52 @@ import { Sale } from './schema/sale.schema';
 
 @Injectable()
 export class SaleService {
-    constructor(
-        @InjectModel(Sale.name) private readonly saleModel: Model<Sale>
-        ) { }
+  constructor(
+    @InjectModel(Sale.name) private readonly saleModel: Model<Sale>
+  ) {}
 
-async addSale(payload:CreateSaleDto){
+  async addSale(payload: CreateSaleDto) {
     const saveItem = new this.saleModel(payload);
-    return await saveItem.save()
-}
-async getSaleById(id: string) {
-    try {
-        const find = await  this.saleModel.findById(id).exec();
-        if (!find) {
-            throw new NotFoundException(`Item with ID ${id} not found`);
-        }
+    return await saveItem.save();
+  }
 
-    return find;
-    } catch (error) {
+  async getSaleById(id: string) {
+    try {
+      const find = await this.saleModel.findById(id).exec();
+      if (!find) {
         throw new NotFoundException(`Item with ID ${id} not found`);
-    
-    }
-}
+      }
 
-async getAllSale(): Promise<Sale[]> {
-    try {
-        const findAll = await this.saleModel.find().exec();
-        return findAll;
+      return find;
     } catch (error) {
-        throw new NotFoundException('Failed to Retrieve Item')
+      throw new NotFoundException(`Item with ID ${id} not found`);
     }
-}
+  }
 
-async updateSale(id: string, payload) {
+  async getAllSale(): Promise<Sale[]> {
     try {
-        const updateSale = await this.saleModel.findById(id).exec();
-
-        if (!updateSale) {
-            throw new NotFoundException(`Item with ID ${id} not found`);
-        }
-
-        Object.assign(updateSale, payload);
-
-        await updateSale.save();
+      const findAll = await this.saleModel.find().exec();
+      return findAll;
+    } catch (error) {
+      throw new NotFoundException('Failed to Retrieve Item');
     }
-}
+  }
 
+  async updateSale(id: string, payload) {
+    try {
+      const updateSale = await this.saleModel.findById(id).exec();
+
+      if (!updateSale) {
+        throw new NotFoundException(`Item with ID ${id} not found`);
+      }
+
+      Object.assign(updateSale, payload);
+
+      await updateSale.save();
+    } catch (error) {
+      throw new NotFoundException(`Failed to Update Item with ID ${id}`);
+    }
+  }
 }
 
 export class SaleProvider {}
